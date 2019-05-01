e85f6e841ca0c78de34cf17cac155eafddfae00b
KirGorod <kgorodisy@gmail.com>
2019-05-01 18:46:43 +0300

    add files

--- functions.py
+++ functions.py
@@ -29,25 +29,34 @@ def play(user_name, chat_id):
     db_date = shelve.open("date_{}".format(chat_id))
     db_game = shelve.open("game_{}".format(chat_id))
     game_list = []
+    now = datetime.now().date()
 
     if user_name not in db_game:
-        return False
-    else:
+        return False, "Участник не зарегистрирован!\nНапишите /reg для регистрации.",
+    elif str(now) not in db_date:
         for k in db_game.keys():
             game_list.append(k)
 
         target = random.choice(game_list)
 
-        now = datetime.now().date()
-
         if str(now) not in db_date:
             db_date[str(now)] = target
             count = db_game.pop(target) + 1
             db_game[target] = count
+        db_responses = shelve.open("db_responses")
+        response_copy = db_responses.pop(str(random.randint(0, len(db_responses)-1)))
+
+        return True, target, response_copy
+    else:
+        target = db_date[str(now)]
+        return False, f"Согласно моей информации, по результатам сегодняшнего розыгрыша пидор дня - @{target}"
+
+
+
 
     db_game.close()
     db_date.close()
-    return target
+
 
 def stats(chat_id):
     db_game = shelve.open("game_{}".format(chat_id))
@@ -55,6 +64,23 @@ def stats(chat_id):
     for k,v in db_game.items():
         stat.update({k:v})
     return stat
+
+def today(chat_id):
+    db_date = shelve.open("date_{}".format(chat_id))
+    now = datetime.now().date()
+    if str(now) in db_date:
+        target = db_date[str(now)]
+        return True, target
+    db_date.close()
+
+def add(payload):
+    db_responses = shelve.open("db_responses")
+    payload = payload[5:].split("/")
+    db_responses[str(len(db_responses))] = payload
+    id = len(db_responses)
+    db_responses.close()
+
+    return id
 
 
 #register(input("Enter user"))
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import telebot
 import functions
 from constants import TOKEN
+import time
+
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    bot.send_message(message.chat.id, "Вас приветствует @dolbaeb_bot!\n"
+                                      "\nМеня создали чисто по фану, моё существование не имеет никакого смысла, кроме развлечение отбитых кожаных ублюков вроде тебя :)\n\n"
+                                      "Можешь попробовать посмотреть список команд написав /help")
+
+@bot.message_handler(commands=['help'])
 def command_help(message):
     bot.send_message(message.chat.id, "Список команд:\n"
-                          "/start - start\n"
-                          "/help - help\n"
-                                      "/roll - Случайное число от 0 до 100\t/roll {number}\n"
-                                      "/flip")
+                          "/help - Список команд\n"
+                                      "/roll - Случайное число от 0 до 100\t\t\t/roll {number}\n"
+                                      "/flip - Орёл/решка.\n"
+                                      "/reg - Регистрация в игре.\n"
+                                      "/play - Запустить игру.\n"
+                                      "/add - Добавить реплику для игры\nСообщения расделять знаком '^', ник для сообщения указать '@{}'\t\t\t /add Msg1 ^ msg2 ^ msg3 @{}")
 
 @bot.message_handler(commands=['flip'])
 def command_flip(message):
@@ -42,20 +52,32 @@ def command_register(message):
 def command_play(message):
     chat_id = message.chat.id
     user_name = message.from_user.username
-    if functions.play(user_name, chat_id) == False:
-        bot.send_message(message.chat.id, "Участник не зарегистрирован!\nНапишите /reg для регистрации.")
-    else:
-        target = functions.play(user_name, chat_id)
-        bot_message = 'Сегодняшний герой у нас @' + target
-        bot.send_message(message.chat.id, bot_message)
+    num = functions.play(user_name,chat_id)
+    if num[0] == False:
+        bot.send_message(message.chat.id, num[1])
+    elif num[0] == True:
+        response = num[2]
+        target = num[1]
+        for i in response:
+            time.sleep(1)
+            bot.send_message(message.chat.id, i.format(target))
+
 
 @bot.message_handler(commands=["stats"])
 def command_stats(message):
     chat_id = message.chat.id
     data = functions.stats(chat_id)
+    s = ""
     for k,v in data.items():
-        bot.send_message(message.chat.id, str(k) + " is " + str(v) + " times\n")
+        s = s + str(k) + " был пидаром " + str(v) + " раз\n"
+    stat_msg = "Топ пидаров:\n\n" + s + "\nВсего участников - {}".format(len(data))
+    bot.send_message(message.chat.id, stat_msg)
 
+@bot.message_handler(commands=["add"])
+def command_add(message):
+    text = message.text
+    response_id = functions.add(text)
+    bot.send_message(message.chat.id, "Сообщение добавлено! Уникальный идентификатор: " + str(response_id))
 
 bot.polling()
 
